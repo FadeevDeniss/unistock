0256981c08e3386025a48e3e751220919b126205
FadeevDeniss <deniro094@gmail.com>
2023-01-30 19:58:18 +0300

    initial commit

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.views.generic.base import TemplateView
+
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        pass
+
+
